fb00d9346be11a2e3a5cfcb26eb89465d2896995
Hellay1218 <hoseinhooshyar12@gmail.com>
2025-01-22 23:05:22 +0330

    clean up 2

--- src/main/java/net/hellay/xp_containers/config/XpContainersConfig.java
+++ src/main/java/net/hellay/xp_containers/config/XpContainersConfig.java
@@ -8,9 +8,8 @@ public class XpContainersConfig extends MidnightConfig {
     @Comment(centered = true) public static Comment disclaimer1;
     @Comment(centered = true) public static Comment disclaimer2;
     @Comment() public static Comment header;
-    @Comment() public static Comment xpStateExplanation;
     @Entry() public static XpStateEnum xpStateEnum = XpStateEnum.POINTS;
-
+    @Comment() public static Comment xpStateExplanation;
     @Entry(isSlider = true, min = 8, max = 64) public static int xpBottleMaxXp = 16;
     @Entry(isSlider = true, min = 16, max = 128) public static int xpJarMaxXp = 64;
 
